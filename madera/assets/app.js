8a170a485bc74ac38109e6a7b826bbbab63df94d
XXXalanXXX <32535732+XXXalanXXX@users.noreply.github.com>
2020-10-21 15:14:53 +0200

    routing + navbar

--- madera/assets/app.js
+++ madera/assets/app.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons, NavbarPlugin } from 'bootstrap-vue';
+
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(NavbarPlugin)
+
 import App from './js/App.vue'
-console.log(App)
+
 new Vue({
     el: "#app",
     components : {App},
+
+
 })
--- madera/assets/js/App.vue
+++ madera/assets/js/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <h2>Welcome Home</h2>
-    <button @click="counter++">Click me {{counter}}</button>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" :style="'width:'+counter+'%'" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <img :src="'img/logo_madera.png'" alt="#" style="width: 20%">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :href="'login'">Modules</b-nav-item>
+          <b-nav-item :href="'Gammes'" >Gammes</b-nav-item>
+          <b-nav-item :href="'Huisseries'" >Huisseries</b-nav-item>
+          <b-nav-item :href="'Montants'" >Montants</b-nav-item>
+          <b-nav-item :href="'Sections'" >Sections</b-nav-item>
+          <b-nav-item :href="'Composants'" >Composants</b-nav-item>
+          <b-nav-item :href="'Fournisseurs'" >Fournisseurs</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
 <script>
-  export default{
-    data: () => ({
-      counter:0
-    })
-  }
+
 </script>
 
 <style>

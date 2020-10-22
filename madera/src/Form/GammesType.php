18b8f1a26d7153a55e54ea8b6dddff473f871be9
Alexy-Auffret <alexyauffret@gmail.com>
2020-10-22 15:20:19 +0200

    Crud Gammes

--- madera/src/Form/GammesType.php
+++ madera/src/Form/GammesType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Gammes;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,36 @@ class GammesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle_gamme')
-            ->add('finition_exterieur')
-            ->add('type_isolant')
-            ->add('type_couverture')
-            ->add('qualite_huisserie')
+            ->add('libelle_gamme', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Libellé de la gamme",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
+            ->add('finition_exterieur', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Finitions extèrieure",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
+            ->add('type_isolant', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Type d'isolant",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
+            ->add('type_couverture', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Type de couverture",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
+            ->add('qualite_huisserie', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Qualité des huisseries",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
         ;
     }
 

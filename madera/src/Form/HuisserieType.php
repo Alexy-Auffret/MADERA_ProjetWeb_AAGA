62687b5da018ff94cdb4307fd1ba92cb61022a34
Alexy-Auffret <alexyauffret@gmail.com>
2020-10-22 14:39:36 +0200

    Modification du crud huisserie

--- madera/src/Form/HuisserieType.php
+++ madera/src/Form/HuisserieType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Huisserie;
+use App\Entity\Modules;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,12 @@ class HuisserieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libelle_huisserie')
-            ->add('modules')
+            ->add('libelle_huisserie', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Libellé de l'huisserie",
+                    'class' => 'form-control mt-2',
+                ]
+            ])
         ;
     }
 
